ac5d18d97d8d00cd8f83ddf88e15a1876f54a969
luxmikaanth <107178139+luxmikaanth@users.noreply.github.com>
2022-10-20 18:10:24 +0530

    Delete Backend directory

--- Backend/AdharAdminProtal/src/main/java/com/admin/Repository/adminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.admin.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.admin.model.admin;
-
-public interface adminRepository extends JpaRepository<admin, Integer> {
-
-}
